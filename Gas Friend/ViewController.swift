2ad0ec996112fd027e7e728319493d881f4e025c
Griffin Jones <dj536914@ohio.edu>
2017-10-26 11:18:01 -0400

    Initial Commit

---
+++ Gas Friend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gas Friend
+//
+//  Created by Griffin Jones on 10/26/17.
+//  Copyright © 2017 Team Best Team #1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
