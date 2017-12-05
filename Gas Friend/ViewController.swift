2eb8ffecc16c013b34dda5923bf6590ecdb5a2e7
Brad Martin <brad@Brads-iMac.local>
2017-12-04 18:05:00 -0800

    Fixed my error

--- Gas Friend/ViewController.swift
+++ Gas Friend/ViewController.swift
@@ -8,31 +8,26 @@
 
 import UIKit
 
-<<<<<<< HEAD
-class ViewController: UIViewController, UITextFieldDelegate {
-=======
+
+class ViewController: UIViewController {
 
 
 class ViewController: UIViewController {
     
->>>>>>> 1971a11a1f68196baeb9699085daaada86f640a5
-
     @IBOutlet weak var Odom: UITextField!
     @IBOutlet weak var Gas: UITextField!
     @IBOutlet weak var Output: UILabel!
     
     @IBAction func Convert(_ sender: Any) {
-        var a = (Odom.text! as NSString).doubleValue
-        var b = (Gas.text! as NSString).doubleValue
-        var answer = a/b
+        let a = (Odom.text! as NSString).doubleValue
+        let b = (Gas.text! as NSString).doubleValue
+        let answer = a/b
         Output.text = "\(answer)"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Odom.delegate = self
-        Gas.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
